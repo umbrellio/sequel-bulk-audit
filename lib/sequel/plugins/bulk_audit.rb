4c3a3cd5dee29ac5768adbd004b655e906c046e3
Fox <why@not.com>
2018-06-08 15:30:00 +0300

    new version of audit gem

--- lib/sequel/plugins/bulk_audit.rb
+++ lib/sequel/plugins/bulk_audit.rb
@@ -20,17 +20,15 @@ module Sequel
 
         def with_current_user(current_user, attributes = nil)
           self.db.transaction do
-            if self.db.table_exists?(:__audit_info)
-              yield if block_given?
-              return
-            end
+            trid = self.db.select(Sequel.function(:txid_current)).single_value
+            self.db.drop_table?(:"__audit_info_#{trid}")
             data = self.db.select(Sequel.expr(current_user&.id || 0).as(:user_id),
                              Sequel.cast(current_user&.login || "unspecified", :text).as(:username),
                              Sequel.pg_jsonb(model_to_table_map).as(:model_map),
                              Sequel.pg_jsonb(attributes || {}).as(:data))
-            self.db.create_table!(:__audit_info, temp: true, as: data)
+            self.db.create_table!(:"__audit_info_#{trid}", temp: true, as: data)
             result = yield if block_given?
-            self.db.drop_table?(:__audit_info)
+            self.db.drop_table?(:"__audit_info_#{trid}")
             result
           end
         end
